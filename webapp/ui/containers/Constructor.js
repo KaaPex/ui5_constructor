40a29feb4c363e86b3729d94748c1017a0198034
Aleksey Kazantsev <develop.kaa@gmail.com>
2019-01-30 22:47:15 +0300

    renderer

--- webapp/ui/containers/Constructor.js
+++ webapp/ui/containers/Constructor.js
@@ -1,95 +1,136 @@
-sap.ui.define(["sap/ui/core/Control"], function(Control) {
-  "use strict";
-  
-  return Control.extend("evola.ui.containers.Constructor", {
-    metadata: {
-      interfaces : ["sap.ui.core.IFormContent"],
-      properties: {
-        name: {
-          type: "string",
-          group: "Misc",
-          defaultValue: ""
+sap.ui.define(
+  ["sap/ui/core/Control", "jquery.sap.global", "sap/ui/layout/library"],
+  function(Control, jQuery, library) {
+    "use strict";
+
+    // shortcut for sap.ui.layout.form.SimpleFormLayout
+    var SimpleFormLayout = library.form.SimpleFormLayout;
+
+    var ResponsiveLayout;
+    var ResponsiveFlowLayoutData;
+    var ResponsiveGridLayout;
+    var GridLayout;
+    var GridContainerData;
+    var GridElementData;
+    var ColumnLayout;
+
+    return Control.extend("evola.ui.containers.Constructor", {
+      metadata: {
+        interfaces: ["sap.ui.core.IFormContent"],
+        properties: {
+          name: {
+            type: "string",
+            group: "Misc",
+            defaultValue: ""
+          },
+          title: "Evola Form Constructor",
+          width: {
+            type: "sap.ui.core.CSSSize", //this is optional, but it helps prevent errors in your code by enforcing a type
+            defaultValue: "100%" //this is also optional, but recommended, as it prevents your properties being null
+          },
+          height: {
+            type: "sap.ui.core.CSSSize",
+            defaultValue: "auto"
+          },
+          layout: {
+            type: "sap.ui.layout.form.SimpleFormLayout",
+            group: "Misc",
+            defaultValue: SimpleFormLayout.ResponsiveLayout
+          },
+          editable: {
+            type: "boolean",
+            group: "Behavior",
+            defaultValue: true
+          }
         },
-        title: "Evola Form Constructor",
-        width: {
-          type: "sap.ui.core.CSSSize", //this is optional, but it helps prevent errors in your code by enforcing a type
-          defaultValue: "100%" //this is also optional, but recommended, as it prevents your properties being null
+        aggregations: {
+          items: {
+            type: "evola.ui.containers.ConstructorItem",
+            multiple: true,
+            singularName: "item",
+            bindable: "bindable"
+          }
         },
-        height: {
-          type: "sap.ui.core.CSSSize",
-          defaultValue: "auto"
-        }
-      },
-      aggregations: {
-        items: {
-          type: "evola.ui.containers.ConstructorItem",
-          multiple: true,
-          singularName: "item",
-          bindable: "bindable"
-        }
-      },
-      associations: {
-        selectedItem: {
-          type: "evola.ui.containers.ConstructorItem",
-          multiple: false
-        }
-      },
-      defaultAggregation: "items",
-      events: {
-        change: {
-          parameters: {
-            selectedItem: {
-              type: "evola.ui.containers.ConstructorItem"
+        associations: {
+          selectedItem: {
+            type: "evola.ui.containers.ConstructorItem",
+            multiple: false
+          }
+        },
+        defaultAggregation: "items",
+        events: {
+          change: {
+            parameters: {
+              selectedItem: {
+                type: "evola.ui.containers.ConstructorItem"
+              }
             }
           }
         }
-      }
-    },
+      },
 
-    init: function() {
-      if (sap.ui.core.Control.prototype.init) {
-        // check whether superclass implements the method
-        sap.ui.core.Control.prototype.init.apply(this, arguments); // call the method with the original arguments
-      }
+      init: function() {
+        if (sap.ui.core.Control.prototype.init) {
+          // check whether superclass implements the method
+          sap.ui.core.Control.prototype.init.apply(this, arguments); // call the method with the original arguments
+        }
 
-      //var libraryPath = jQuery.sap.getModulePath("evola.ui"); //get the server location of the ui library
-      //jQuery.sap.includeStyleSheet(libraryPath + "/../css/dalrae.css"); //specify the css path relative from the ui folder
-    },
+        //var libraryPath = jQuery.sap.getModulePath("evola.ui"); //get the server location of the ui library
+        //jQuery.sap.includeStyleSheet(libraryPath + "/../css/dalrae.css"); //specify the css path relative from the ui folder
+      },
 
-    renderer: function(oRm, oControl) {
-      //first up, render a div for the ShadowBox
-      oRm.write("<div");
-      //render width & height properties
-      oRm.write(
-        ' style="width: ' +
-          oControl.getWidth() +
-          "; height: " +
-          oControl.getHeight() +
-          ';"'
-      );
-      //next, render the control information, this handles your sId (you must do this for your control to be properly tracked by ui5).
-      oRm.writeControlData(oControl);
-      oRm.addClass("Constructor");
-      oRm.writeClasses();
-      oRm.write(">");
+      renderer: function(oRm, oControl) {
+        var oLayout = oControl.getLayout();
 
-      var aItems = oControl.getItems();
+        oRm.write("<div");
+        //render width & height properties
+        oRm.write(
+          ' style="width: ' +
+            oControl.getWidth() +
+            "; height: " +
+            oControl.getHeight() +
+            ';"'
+        );
+        //next, render the control information, this handles your sId (you must do this for your control to be properly tracked by ui5).
+        oRm.writeControlData(oControl);
+        oRm.addClass("sapUiSimpleForm");
+        if (oControl.getEditable()) {
+          oRm.addClass("sapUiFormEdit");
+          oRm.addClass("sapUiFormEdit-CTX");
+        }
+        oRm.writeStyles();
+        oRm.writeClasses();
+        oRm.write(">");
 
-      aItems.forEach(item => {
-        oRm.renderControl(item);
-      });
+        var aItems = oControl.getItems();
 
-      //and obviously, close off our div
-      oRm.write("</div>");
-    },
+        // if (oLayout) {
+        //   // render the layout with the content of this form control
+        //   oRm.renderControl(oLayout);
+        // } else {
+        //   jQuery.sap.log.warning(
+        //     'Form "' + oControl.getId() + '" - Layout missing!',
+        //     "Renderer",
+        //     "Constructor"
+        //   );
+        // }
 
-    onAfterRendering: function() {
-      //if I need to do any post render actions, it will happen here
-      if (sap.ui.core.Control.prototype.onAfterRendering) {
-        sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
-      }
-    },
+        aItems.forEach(item => {
+          oRm.renderControl(item);
+        });
 
-    exit: function() {}
-  });
-});
+        //and obviously, close off our div
+        oRm.write("</div>");
+      },
+
+      onAfterRendering: function() {
+        //if I need to do any post render actions, it will happen here
+        if (sap.ui.core.Control.prototype.onAfterRendering) {
+          sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
+        }
+      },
+
+      exit: function() {}
+    });
+  }
+);
--- webapp/ui/containers/ConstructorItem.js
+++ webapp/ui/containers/ConstructorItem.js
@@ -1,135 +1,143 @@
-sap.ui.define(["sap/ui/core/Control", "sap/ui/core/library"], function(
-  Control,
-  coreLibrary
-) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/Control", "sap/ui/core/library", "sap/ui/layout/library", "sap/ui/layout/form/ColumnLayout", "sap/ui/layout/form/ColumnLayout"],
+  function(Control, coreLibrary, library, ColumnLayout, ColumnLayoutRenderer) {
+    "use strict";
 
-  // shortcut for sap.ui.core.ValueState
-  let ValueState = coreLibrary.ValueState;
+    // shortcut for sap.ui.core.ValueState
+    let ValueState = coreLibrary.ValueState;
+    var SimpleFormLayout = library.form.SimpleFormLayout;
 
-  return Control.extend("evola.ui.containers.ConstructorItem", {
-    metadata: {
-      properties: {
-        name: {
-          type: "string",
-          group: "Misc",
-          defaultValue: ""
+    return Control.extend("evola.ui.containers.ConstructorItem", {
+      metadata: {
+        properties: {
+          name: {
+            type: "string",
+            group: "Misc",
+            defaultValue: ""
+          },
+          label: {
+            type: "string",
+            group: "Misc",
+            defaultValue: null
+          },
+          type: {
+            type: "string",
+            group: "Misc",
+            defaultValue: ""
+          },
+          value: {
+            type: "string",
+            group: "Data",
+            defaultValue: null,
+            bindable: "bindable"
+          },
+          enabled: {
+            type: "boolean",
+            group: "Behavior",
+            defaultValue: true
+          },
+          editable: {
+            type: "boolean",
+            group: "Behavior",
+            defaultValue: true
+          },
+          placeholder: {
+            type: "string",
+            group: "Misc",
+            defaultValue: null
+          },
+          valueState: {
+            type: "sap.ui.core.ValueState",
+            group: "Data",
+            defaultValue: ValueState.None
+          },
+          required: {
+            type: "boolean",
+            group: "Misc",
+            defaultValue: false
+          }
         },
-        label: {
-          type: "string",
-          group: "Misc",
-          defaultValue: ""
-        },
-        type: {
-          type: "string",
-          group: "Misc",
-          defaultValue: ""
-        },
-        value: {
-          type: "string",
-          group: "Data",
-          defaultValue: null,
-          bindable: "bindable"
-        },
-        enabled: {
-          type: "boolean",
-          group: "Behavior",
-          defaultValue: true
-        },
-        editable: {
-          type: "boolean",
-          group: "Behavior",
-          defaultValue: true
-        },
-        placeholder: {
-          type: "string",
-          group: "Misc",
-          defaultValue: null
-        },
-        valueState: {
-          type: "sap.ui.core.ValueState",
-          group: "Data",
-          defaultValue: ValueState.None
-        },
-        required: {
-          type: "boolean",
-          group: "Misc",
-          defaultValue: false
+        events: {
+          change: {
+            parameters: {
+              value: { type: "string" }
+            }
+          },
+          liveChange: {
+            parameters: {
+              value: { type: "string" },
+              escPressed: { type: "boolean" },
+              previousValue: { type: "string" }
+            }
+          }
         }
       },
-      events: {
-        change: {
-          parameters: {
-            value: { type: "string" }
-          }
-        },
-        liveChange : {
-          parameters : {
-            value : {type : "string"},
-            escPressed : {type : "boolean"},
-            previousValue : {type : "string"}
-          }
-        },
-      }
-    },
-    onChange: function(oEvent) {
-      // check the control is editable or not
-      if (!this.getEditable() || !this.getEnabled()) {
-        return;
-      }
+      onChange: function(oEvent) {
+        // check the control is editable or not
+        if (!this.getEditable() || !this.getEnabled()) {
+          return;
+        }
 
-      var sValue = oEvent.getParameter("value");
+        var sValue = oEvent.getParameter("value");
 
-      if (sValue !== this._lastValue) {
-        this._lastValue = sValue;
-        this.fireChange(oEvent.getParameters());
-        return true;
-      }
-    },
-    oninput: function(oEvent) {
-      this.fireLiveChange(oEvent.getParameters());
-    },
-    init: function() {},
-    renderer: function(oRm, oControl) {
-      let label = new sap.m.Label({
-        text: oControl.getName()
-      });
-      oRm.renderControl(label);
-      let input = new sap.m.Input({
-        valueHelpOnly: true,
-        value: oControl.getValue(),
-        name: oControl.getName(),
-        change: oControl.onChange.bind(oControl),
-        liveChange: oControl.oninput.bind(oControl)
-      });
-      oRm.renderControl(input);
+        if (sValue !== this._lastValue) {
+          this._lastValue = sValue;
+          this.fireChange(oEvent.getParameters());
+          return true;
+        }
+      },
+      oninput: function(oEvent) {
+        this.fireLiveChange(oEvent.getParameters());
+      },
+      init: function() {
+        
+      },
+      renderer: function(oRm, oControl) {
+        //TODO: Need implement something like this https://github.com/SAP/openui5/blob/7672ca39cd0fdaf9e822172dd52e5d861aa0bd12/src/sap.ui.layout/src/sap/ui/layout/form/ColumnLayoutRenderer.js
+        // oRm.write("<div");
+        // oRm.writeElementData(oControl);
+        // oRm.addClass("sapUiFormElement");
+        // if (oControl.getLabel()) {
+        //   oRm.addClass("sapUiFormElementLbl");
+        // }
+        // oRm.writeClasses();
+        // oRm.write(">");
+        let input = new sap.m.Input({
+          valueHelpOnly: true,
+          value: oControl.getValue(),
+          name: oControl.getName(),
+          change: oControl.onChange.bind(oControl),
+          liveChange: oControl.oninput.bind(oControl)
+        });
 
-      // oRm.write("<div");
-      // oRm.writeControlData(oControl);
-      // oRm.addClass("ConstructorItem");
-      // oRm.writeClasses();
-      // oRm.write(">");
-      //
-      // oRm.write("<label");
-      // oRm.writeAttributeEscaped("htmlFor", oControl.getName());
-      // oRm.write(">");
-      // oRm.write(oControl.getLabel());
-      // oRm.write("</label>");
-      //
-      // oRm.write("<input");
-      // oRm.writeAttributeEscaped("htmlFor", oControl.getName());
-      // oRm.writeAttributeEscaped("type", "text");
-      // oRm.writeAttributeEscaped("name", oControl.getName());
-      // oRm.writeAttributeEscaped("value", oControl.getValue());
-      // oRm.write("/>");
-      //
-      // oRm.write("</div>");
-    },
-    onAfterRendering: function() {
-      //if I need to do any post render actions, it will happen here
-      if (sap.ui.core.Control.prototype.onAfterRendering) {
-        sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
+        let oElement = new sap.ui.layout.form.FormElement();
+        oElement.addField(input);
+        if (oControl.getLabel()) {
+          let label = new sap.m.Label({
+            text: oControl.getLabel()
+          });
+          label.setLabelFor(input);
+          oElement.setLabel(label);
+          // oRm.renderControl(label);
+        }
+
+        let oContainer = new sap.ui.layout.form.FormContainer(oControl.getId() + "--Container");
+        oContainer.addFormElement(oElement);
+        let oLayout = new ColumnLayout(oContainer.getId() + "--Layout");
+        oLayout.setParent(oContainer, null, true);
+        
+        let renderer = ColumnLayoutRenderer.prototype.getRenderer();
+        renderer.renderElement(oRm, oLayout, oElement);
+
+        // oRm.renderControl(input);
+        // oRm.write("</div>");
+      },
+      onAfterRendering: function() {
+        //if I need to do any post render actions, it will happen here
+        if (sap.ui.core.Control.prototype.onAfterRendering) {
+          sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
+        }
       }
-    }
-  });
-});
+    });
+  }
+);
